13d80c15bc2237e7954e93bd7d42520383c0b39c
Minkhant21 <kaungminkhant6889@gmail.com>
2023-10-06 11:42:44 +0630

    404

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const http = require('http');
 
 const express = require('express');
+const adminRoute = require('./routes/admin')
 
 const app = express();
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(adminRoute)
 
-app.use('/',(req,res,next)=>{
-    console.log('one middleware!')
-    next()
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
-app.use('/users',(req,res,next)=>{
-    console.log('two middleware!')
-    res.send('<p>users</p>')
-})
-
 
 app.listen(3000,()=>{
     console.log('server Running 3000')
