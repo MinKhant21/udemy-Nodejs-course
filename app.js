42b1a128003e2e05e6398ad5940d6c56caa79e52
Minkhant21 <kaungminkhant6889@gmail.com>
2023-10-08 14:59:33 +0630

    templateEngine

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const http = require('http');
-
 const express = require('express');
-const adminRoute = require('./routes/admin')
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+const engine = require('express-edge')
+const path = require('path');
 
 const app = express();
+
+// Automatically sets view engine and adds dot notation to app.render
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(adminRoute)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin/',adminRoute)
+app.use(shopRoute)
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).render('404')
 })
 
 app.listen(3000,()=>{
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,12 @@
 const express = require('express')
-
 const router = express.Router()
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="product"></input><input type="submit" value="Add Product"></input></form>')
+    res.render('add-product')
 })
 
 router.post('/product',(req,res,next)=>{
     console.log(req.body)
 })
-
 module.exports = router;
 
